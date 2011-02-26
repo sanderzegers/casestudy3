2a3cef31089517dc683734500c1371fe74a40750
Sander Zegers <mail@sanderzegers.com>
2011-02-26 18:20:12 +0100

    Warenkorb update. IE Fix.

---
+++ application/controllers/cart.php
@@ -0,0 +1,121 @@
+<?php
+/*
+ * Created on Feb 17, 2011
+ *
+ * To change the template for this generated file go to
+ * Window - Preferences - PHPeclipse - PHP - Code Templates
+ */
+ 
+ class Cart extends CI_Controller{
+ 	
+ 	
+ 	// Needed for UTF-8 serialization issues
+ 	function mb_unserialize($serial_str) {
+		$out = preg_replace('!s:(\d+):"(.*?)";!se', "'s:'.strlen('$2').':\"$2\";'", $serial_str );
+		return unserialize($out);
+	} 
+ 
+ 	function __construct()
+		{
+		parent::__construct();
+		
+		$this->load->helper('url_helper');
+		//TODO: Fix path
+		include("application/libraries/Articleclass.php");
+		include("application/libraries/CartClass.php");
+				
+		$temp = unserialize($this->session->userdata('myCart'));
+
+		if(!(is_object($temp) && ($temp instanceof CartClass))){
+			$myCart = new CartClass;			
+			$this->session->set_userdata(array("myCart" => serialize($myCart)));		
+			}
+		}
+		
+		
+	public function show(){
+		$this->load->view('head/standard');
+		$this->load->model('menu');
+		$menuData = ($this->menu->getMenu());
+		$this->load->view('content_left/standard',array("menu" => $menuData));
+		
+		$myCart = (unserialize($this->session->userdata('myCart')));
+		
+		$this->load->view('content_center/cartcontent',array("title" => "Warenkorb","myCart" => $myCart));
+		$this->load->view('content_right/standard');
+		$this->load->view('foot/standard');
+	}
+	
+
+	
+	public function add(){
+		
+		$sourceSite = $this->input->post('currentSite');
+		$artikel = $this->mb_unserialize($this->input->post('article'));
+		$myCart = (unserialize($this->session->userdata('myCart')));
+
+		
+		$myCart->add($artikel,1);
+		$this->session->set_userdata(array("myCart" => serialize($myCart)));
+		redirect($sourceSite);
+	}
+	
+	public function action(){
+		$sourceSite = $this->input->post('currentSite');
+		$actionType = $this->input->post('actionType');
+		$artikel = $this->mb_unserialize($this->input->post('article'));
+		$myCart = (unserialize($this->session->userdata('myCart')));
+		
+		$add = (boolean)$this->input->post('add');
+		$subtract = (boolean)$this->input->post('subtract');
+		$remove = (boolean)$this->input->post('remove');
+
+		
+		switch($actionType){
+			case "add":
+				$myCart->changeAmount($artikel,1);
+				break;
+				
+			case "subtract":
+				$myCart->changeAmount($artikel,-1);
+				break;
+				
+			case "remove":
+				$myCart->remove($artikel);
+				break;
+		}
+		
+		$this->session->set_userdata(array("myCart" => serialize($myCart)));
+		
+		
+		
+		redirect($sourceSite);
+		
+	}
+	
+//	public function remove(){
+//		$sourceSite = $this->input->post('currentSite');
+//		$artikel = $this->mb_unserialize($this->input->post('article'));
+//		
+//		$myCart = (unserialize($this->session->userdata('myCart')));
+//		
+//		
+//		redirect($sourceSite);
+//		
+//	}
+	
+	
+	public function destroy(){
+
+		$myCart = (unserialize($this->session->userdata('myCart')));
+		$myCart->destroy();
+		$this->session->set_userdata(array("myCart" => serialize($myCart)));
+		
+		redirect($this->input->post('currentSite'));
+		
+	}
+	
+		
+ }
+ 
+?>
---
+++ application/views/content_center/cartcontent.php
@@ -0,0 +1,56 @@
+<?
+$currency = $this->config->item('currency');
+$templateImage = $this->config->item('templateImage');
+
+$i = 1;
+
+$this->load->helper('form_helper');
+
+?>
+
+<div id="content_center">
+<p id="content_center_title">
+        <h1> <?=$title?> </h1>
+        </p>
+		<? foreach ($myCart->getContent() as $position): ?>
+        
+        <? //var_dump($position); ?>
+        <div id="content_center_wk">
+          <table width="527" border="0">
+            <tr>
+              <td width="20"><? echo $i++;?></td>
+
+              <td width="277"><?= $position["artikel"]->bezeichnung?></td>
+              <? echo form_open('cart/action');?>
+              <input type="hidden" name="article" value="<? echo htmlspecialchars(serialize($position["artikel"]))?>" />
+			  <input type="hidden" name="currentSite" value="<?= current_url()?>" / >
+              <input type="hidden" name="actionType" value="remove"/>
+              <td width="20"><input type="image" src="<?= $templateImage ?>wk_delete.jpg" width="10" height="10"></td>
+              <? echo form_close();?>
+              <? echo form_open('cart/action');?>
+              <input type="hidden" name="article" value="<? echo htmlspecialchars(serialize($position["artikel"]))?>" />
+			  <input type="hidden" name="currentSite" value="<?= current_url()?>" / >
+			  <input type="hidden" name="actionType" value="add"/>
+              <td width="20"><input type="image" src="<?= $templateImage ?>wk_add.jpg" width="10" height="10"></td>
+              <? echo form_close();?>
+              <? echo form_open('cart/action');?>
+              <input type="hidden" name="article" value="<? echo htmlspecialchars(serialize($position["artikel"]))?>" />
+			  <input type="hidden" name="currentSite" value="<?= current_url()?>" / >
+			  <input type="hidden" name="actionType" value="subtract"/>
+              <td width="20"><input type="image" src="<?= $templateImage ?>wk_rem.jpg" width="10" height="10"></td>
+              <? echo form_close();?>
+              <td width="70"><?= $position["menge"]?>. Stk</td>
+              <td width="70"><? echo $currency." ".sprintf("%01.2f", $position["artikel"]->preis*$position["menge"])?></td>
+            </tr>
+
+          </table>
+        </div>
+        <div id="content_center_wk_footer">
+        &nbsp;
+        </div>
+        <?php endforeach?>
+          Total: <?= $myCart->getTotalValue()?>
+          <br>
+          <a href="<?=site_url('cart/destroy')?>">Warenkorb leeren</a>
+	  	  <p>Kaufen!</p>
+	  </div>
